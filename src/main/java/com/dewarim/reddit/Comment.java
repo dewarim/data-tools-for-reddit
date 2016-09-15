835d1164877d6aa3a19bd4a723f5fb0a47216188
Ingo Wiarda <iwiarda@connexity.com>
2016-09-15 13:49:44 +0200

    Add non-static createLink method variant. Add default values for integer fields to make conversion easier if JSON version does not contain a specifc field.
    (For example, insertion into database with non-null columns is easier if comment.downs is 0 instead of null)

--- src/main/java/com/dewarim/reddit/Comment.java
+++ src/main/java/com/dewarim/reddit/Comment.java
@@ -2,67 +2,54 @@ package com.dewarim.reddit;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+
 /**
  * A Reddit comment.
  */
-@JsonPropertyOrder(value = {"author", "name", "body", "author_flair_text", "gilded", "score_hidden", "score", "link_id",
-        "retrieved_on", "author_flair_css_class", "subreddit", "edited", "ups", "downs", "controversiality",
-        "created_utc", "parent_id", "archived", "subreddit_id", "id", "distinguished"})
+@JsonPropertyOrder(value = { "author", "name", "body", "author_flair_text", "gilded", "score_hidden", "score", "link_id", "retrieved_on",
+                             "author_flair_css_class", "subreddit", "edited", "ups", "downs", "controversiality", "created_utc", "parent_id", "archived",
+                             "subreddit_id", "id", "distinguished" })
 public class Comment {
 
-    public String  author;
-    public String  name;
-    public String  body;
-    public String  author_flair_text;
-    public Integer gilded;
-    public Boolean score_hidden;
-    public Integer score;
-    public String  link_id;
-    public Long    retrieved_on;
-    public String  author_flair_css_class;
-    public String  subreddit;
-    public String  edited;
-    public Integer ups;
-    public Integer downs;
-    public Integer controversiality;
-    public Long    created_utc;
-    public String  parent_id;
-    public Boolean archived;
-    public String  subreddit_id;
-    public String  id;
-    public String  distinguished;
+   public String  author;
+   public String  name;
+   public String  body;
+   public String  author_flair_text;
+   public Integer gilded           = 0;
+   public boolean score_hidden;
+   public Integer score            = 0;
+   public String  link_id;
+   public Long    retrieved_on     = 0L;
+   public String  author_flair_css_class;
+   public String  subreddit;
+   public String  edited;
+   public Integer ups              = 0;
+   public Integer downs            = 0;
+   public Integer controversiality = 0;
+   public Long    created_utc      = 0L;
+   public String  parent_id;
+   public boolean archived;
+   public String  subreddit_id;
+   public String  id;
+   public String  distinguished;
 
-    @Override
-    public String toString() {
-        return "Comment{" +
-                "author='" + author + '\'' +
-                ", name='" + name + '\'' +
-                ", body='" + body + '\'' +
-                ", author_flair_text='" + author_flair_text + '\'' +
-                ", gilded=" + gilded +
-                ", score_hidden=" + score_hidden +
-                ", score=" + score +
-                ", link_id='" + link_id + '\'' +
-                ", retrieved_on=" + retrieved_on +
-                ", author_flair_css_class='" + author_flair_css_class + '\'' +
-                ", subreddit='" + subreddit + '\'' +
-                ", edited=" + edited +
-                ", ups=" + ups +
-                ", downs=" + downs +
-                ", controversiality=" + controversiality +
-                ", created_utc=" + created_utc +
-                ", parent_id='" + parent_id + '\'' +
-                ", archived=" + archived +
-                ", subreddit_id='" + subreddit_id + '\'' +
-                ", id='" + id + '\'' +
-                ", distinguished='" + distinguished + '\'' +
-                '}';
-    }
+   @Override
+   public String toString() {
+      return "Comment{" + "author='" + author + '\'' + ", name='" + name + '\'' + ", body='" + body + '\'' + ", author_flair_text='" + author_flair_text + '\''
+         + ", gilded=" + gilded + ", score_hidden=" + score_hidden + ", score=" + score + ", link_id='" + link_id + '\'' + ", retrieved_on=" + retrieved_on
+         + ", author_flair_css_class='" + author_flair_css_class + '\'' + ", subreddit='" + subreddit + '\'' + ", edited=" + edited + ", ups=" + ups
+         + ", downs=" + downs + ", controversiality=" + controversiality + ", created_utc=" + created_utc + ", parent_id='" + parent_id + '\'' + ", archived="
+         + archived + ", subreddit_id='" + subreddit_id + '\'' + ", id='" + id + '\'' + ", distinguished='" + distinguished + '\'' + '}';
+   }
 
-    public static String createLink(String subreddit, String linkId, String commentId) {
-        if (subreddit == null || linkId == null || commentId == null) {
-            return "[error]: can only create link from non-null fields.";
-        }
-        return String.format("http://www.reddit.com/r/%s/comments/%s/%s", subreddit, linkId.replace("t3_", ""), commentId);
-    }
+   public String createLink() {
+      return createLink(subreddit, link_id, id);
+   }
+
+   public static String createLink( String subreddit, String linkId, String commentId ) {
+      if ( subreddit == null || linkId == null || commentId == null ) {
+         return "[error]: can only create link from non-null fields.";
+      }
+      return String.format("http://www.reddit.com/r/%s/comments/%s/%s", subreddit, linkId.replace("t3_", ""), commentId);
+   }
 }
