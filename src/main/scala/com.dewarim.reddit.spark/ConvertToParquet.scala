102ed3e8f45b268915b9fabdd7f451568f3ee68c
ingo.wiarda <ingo_wiarda@dewarim.de>
2016-09-19 21:27:50 +0200

    Add Parquet converter. Upgrade to Spark 2.0.0

---
+++ src/main/scala/com.dewarim.reddit.spark/ConvertToParquet.scala
@@ -0,0 +1,45 @@
+package com.dewarim.reddit.spark
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{SQLContext, SaveMode}
+import org.apache.spark.sql.functions._
+
+/**
+  * Convert .json.bz2 input to parquet columnar storage format. 
+  */
+object ConvertToParquet {
+
+  def convert(inputPath:String): Unit = {
+
+    val conf = new SparkConf()
+    // note: if you use Scala 1.6.1,
+    // use one thread to prevent https://issues.apache.org/jira/browse/SPARK-1861 which causes:
+    // java.lang.ArrayIndexOutOfBoundsException: 18002
+    // at org.apache.hadoop.io.compress.bzip2.CBZip2InputStream.recvDecodingTables(CBZip2InputStream.java:730)
+    // when using local[4]
+    val sc = new SparkContext("local[4]", "Reddit Data Tools", conf)
+    sc.hadoopConfiguration.set("mapreduce.input.fileinputformat.input.dir.recursive","true")
+
+    val sqlContext = new SQLContext(sc)
+
+    sqlContext.read.json(inputPath).write.mode(SaveMode.Overwrite).parquet("reddit-parquet")
+
+    val df = sqlContext.read.parquet("reddit-parquet")
+
+    val authorsByUpvote = df.filter("author != '[deleted]'").filter("body != '[deleted]'").filter("body != '[removed]'").groupBy("author").sum("ups").sort(sum("ups").desc)
+    authorsByUpvote.show()
+    sc.stop()
+  }
+
+  def main(args: Array[String]) {
+    val inputPath = if(args.length > 0){
+      args(0)
+    }
+    else{
+      "data/RC_2007-10.bz2"
+    }
+    ConvertToParquet.convert(inputPath)
+  }
+}
+
