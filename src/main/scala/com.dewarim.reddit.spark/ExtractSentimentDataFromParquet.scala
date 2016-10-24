bbe4d0622f547fe75419203243f807cbe55d5ac4
ingo.wiarda <ingo_wiarda@dewarim.de>
2016-10-24 15:59:15 +0200

    Class to extract just the sentiment data (so you can join it on demand to your reddit dataset)

---
+++ src/main/scala/com.dewarim.reddit.spark/ExtractSentimentDataFromParquet.scala
@@ -0,0 +1,36 @@
+package com.dewarim.reddit.spark
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Extract just the comment id and sentiment data from the whole dataset. 
+  */
+object ExtractSentimentDataFromParquet {
+
+  def convert(inputPath:String): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("reddit data tools on spark")
+      .master("local[4]")
+      .getOrCreate()
+
+    val df = spark.read.parquet(inputPath)
+    
+    val sentiment = df.filter("author != '[deleted]'").filter("body != '[deleted]'").filter("body != '[removed]'")
+    .select("id", "roundedFinalScore", "maxPosScore", "maxNegScore")
+    sentiment.show()
+    sentiment.write.parquet("results/reddit-sentiment-2016-08")
+    spark.stop()
+  }
+
+  def main(args: Array[String]) {
+    val inputPath = if(args.length > 0){
+      args(0)
+    }
+    else{
+      "reddit-parquet-sentiment/*/*"
+    }
+    ExtractSentimentDataFromParquet.convert(inputPath)
+  }
+}
+
