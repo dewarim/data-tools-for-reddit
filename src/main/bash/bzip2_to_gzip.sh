bc9af9d4d80db479b356dad9f55925a3a12f6a2c
ingo.wiarda <ingo_wiarda@dewarim.de>
2017-04-08 09:42:23 +0200

    Convert bzip2 files and create sha256sum file to detect file corruption.

--- src/main/bash/bzip2_to_gzip.sh
+++ src/main/bash/bzip2_to_gzip.sh
@@ -7,16 +7,26 @@ set -e
 # CBZip2InputStream is *not* thread-safe, and I am not sure if Spark 2 in standalone mode 
 # handles master.local[1] correctly.
 
+# switch to output directory:
 cd reddit-gz
 
 for path in /var/lib/transmission-daemon/downloads/reddit_data/20*; do
         #echo $path                                                                                                                                                                                                                                                                                                                                                                           
         for file in ${path}/*.bz2; do
-            echo "convert: $file to "
-            simple_name=$(basename $file)
-            gz_name=${simple_name/bz2/gz}
-            echo $gz_name
-            bzcat $file | gzip > ${gz_name}
+          simple_name=$(basename $file)
+          gz_name=${simple_name/bz2/gz}
+          if [[ -e $gz_name ]]; then
+            echo "$gz_name already exists in target path: do not convert again."
+            continue
+          fi
+          
+          # you should compare the hash values with those from 
+          # http://files.pushshift.io/reddit/comments/sha256sums
+          # to detect possible data corruption.
+          echo "create hash value for ${file}"
+          sha256sum $file >> reddit-sha265sums.txt
+          echo "convert file: ${file} to ${gz_name}"
+          bzcat $file | gzip > ${gz_name}
         done
 
 done
